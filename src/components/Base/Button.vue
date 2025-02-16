af6559b0ad104d4c6a0882ad01fea39de37f8174
Xasanov Ibrohim <xasanovdev.work@gmail.com>
2025-02-16 23:14:07 +0500

    feat: dialog

---
+++ src/components/Base/Button.vue
@@ -0,0 +1,57 @@
+<template>
+  <button class="custom-button" :class="[buttonClass, type]">
+    <slot></slot>
+  </button>
+</template>
+
+<script lang="ts" setup>
+import { defineComponent } from 'vue';
+
+
+interface Props {
+    buttonClass?: string;
+    type?: 'primary' | 'secondary' | 'danger';
+}
+
+defineProps<Props>();
+
+defineComponent({
+    name: 'CustomButton'
+})
+
+</script>
+
+<style lang="scss" scoped>
+.custom-button {
+  padding: 10px 20px;
+  border: none;
+  border-radius: 5px;
+  color: white;
+  cursor: pointer;
+  transition: background-color 0.3s;
+
+  &.primary {
+    background-color: #007bff;
+
+    &:hover {
+      background-color: #0056b3;
+    }
+  }
+
+  &.secondary {
+    background-color: #6c757d;
+
+    &:hover {
+      background-color: #5a6268;
+    }
+  }
+
+  &.danger {
+    background-color: #dc3545;
+
+    &:hover {
+      background-color: #c82333;
+    }
+  }
+}
+</style>
--- src/components/Layout/Main.vue
+++ src/components/Layout/Main.vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
+import CustomButton from '../Base/Button.vue';
 import { X } from 'lucide-vue-next';
 import { ref } from 'vue';
 
@@ -7,13 +8,24 @@ const itemsList = ref<Array<{ index: number; isActive: boolean; count: number }>
 itemsList.value = Array.from({ length: 25 }, (_, index) => ({
     index: index + 1,
     isActive: false,
-    count: 0
+    count: 0,
+    color: ''
 }));
+
+const colors = ref(['#7FAA65', '#AA9765', '#656CAA']);
 
 const dialogVisible = ref(false);
 
-const handleClick = (item: { index: number; isActive: boolean; count: number }) => {
+const selectedItem = ref<{ index: number; isActive: boolean; count: number; color: string }>({
+    index: 0,
+    isActive: false,
+    count: 0,
+    color: ''
+});
+
+const handleClick = (item: { index: number; isActive: boolean; count: number; color: string }) => {
     dialogVisible.value = true;
+    selectedItem.value = item;
 };
 
 </script>
@@ -43,15 +55,35 @@ const handleClick = (item: { index: number; isActive: boolean; count: number }) 
             </button>
 
 
-            <div class="dialog" v-if="dialogVisible">
-                <div class="dialog__content">
-                    <h2>Dialog Title</h2>
-                    <p>Dialog content goes here.</p>
-                    <button @click="dialogVisible = false">Close</button>
-                </div>
+            <Transition name="from-right">
+                <div class="dialog" v-if="dialogVisible">
+                    <div class="dialog__content">
+                        <div class="dialog__content-image"
+                            :style="{ backgroundColor: selectedItem?.color }"
+                        >
+                        </div>
 
-                <X class="dashboard__chat-icon" @click="dialogVisible = false" />
-            </div>
+                        <hr class="dialog__content-line">
+
+                        <div class="dialog__content-info">
+                            <div class="dialog__content-info--title shimmer"></div>
+                            <div class="dialog__content-info--subtitle">
+                                <div v-for="i in 4" class="shimmer"></div>
+                            </div>
+                            <div class="dialog__content-info--footer shimmer"></div>
+                        </div>
+
+                        <hr class="dialog__content-line">
+                        
+                        <CustomButton class="delete__button" @click="dialogVisible = false" type="danger">
+                            Удалить предмет
+                        </CustomButton>
+                    </div>
+
+
+                    <X class="dashboard__chat-icon" @click="dialogVisible = false" />
+                </div>
+            </Transition>
         </div>
 
         <div class="dashboard__chat wrapper">
@@ -76,9 +108,86 @@ const handleClick = (item: { index: number; isActive: boolean; count: number }) 
         width: 250px;
         height: 100%;
         display: flex;
-        justify-content: center;
-        align-items: center;
+        flex-direction: column;
         border-left: 1px solid var(--secondary-color);
+
+        &__content {
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            padding: 1rem;
+            width: 100%;
+            height: 100%;
+            padding-top: 55px;
+
+            .delete__button {
+                width: 100%;
+            }
+
+            &-line {
+                width: 100%;
+                height: 1px;
+                background-color: var(--secondary-color);
+                margin-top: 2rem;
+                margin-bottom: 1rem;
+            }
+
+            &-info {
+                margin-top: 20px;
+                width: 100%;
+                display: flex;
+                flex-direction: column;
+                align-items: center;
+                gap: 20px;
+
+            &--title {
+                max-width: 190px;
+                width: 100%;
+                height: 26px;
+                border-radius: 8px;
+            }
+
+            &--subtitle {
+                display: flex;
+                justify-content: center;
+                flex-direction: column;
+                align-items: center;
+                gap: 1rem;
+                width: 100%;
+
+                .shimmer {
+                    width: 88%;
+                    height: 10px;
+                    border-radius: 8px;
+                }
+            }
+
+            &--footer {
+                width: 80px;
+                height: 10px;
+                border-radius: 8px;
+            }
+            }
+
+            &-image {
+                width: 115px;
+                height: 115px;
+                position: relative;
+                background-color: var(--secondary-color);
+                z-index: 2;
+
+                &::before {
+                    content: '';
+                    position: absolute;
+                    top: -14px;
+                    left: 14px;
+                    z-index: 1;
+                    width: 100%;
+                    height: 100%;
+                    backdrop-filter: blur(1rem);
+                }
+            }
+        }
     }
 }
 
